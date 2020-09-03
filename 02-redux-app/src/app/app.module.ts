8f3c4b100450269eac615c182a26505e9e42ba57
Josue <josue.almonasi@hotmail.com>
2020-09-02 19:37:07 -0500

    Added dev tools

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { GrandChildComponent } from './counter/grand-child/grand-child.component
 /* NGRx */
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/reducers/counter.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, ChildComponent, GrandChildComponent],
@@ -14,6 +16,10 @@ import { counterReducer } from './counter/reducers/counter.reducer';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
